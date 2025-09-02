83f7f8180b59d5622e4e3cc3df509e4457102213
enesbilikk <enes.bilik@ogr.gelisim.edu.tr>
2025-09-02 21:38:46 +0300

    add new yolo version

--- yolo_train.py
+++ yolo_train.py
@@ -3,12 +3,19 @@ from ultralytics import YOLO
 def train_model():
 
     model = YOLO('yolov12n.pt')
-    results = model.train(
-        data=r'D:\Medias\dataset_create\for_dataset\data.yaml',
-        epochs=50,
-        imgsz=640,              
-        batch=16,              
-        name="terminal_model_4" ,
+
+    model.train(
+        data=r'C:\Users\PC\Desktop\plates\dataset\data.yaml',
+        epochs=200,
+        imgsz=640,        
+        batch=16,
+        lr0=0.01,
+        lrf=0.2,
+        optimizer='SGD',
+        augment=True,
+        device=0,
+        project='runs/exp',
+        name='plates_yolov12',
     )
 
 if __name__ == "__main__":
